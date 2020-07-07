573f4767ba105bd229978d1a75d848556468ed8b
MoPaMo <mpm1301@web.de>
2020-07-07 21:36:31 +0200

    added colors

--- printWithColor.py
+++ printWithColor.py
@@ -1,4 +1,11 @@
 colorTypes = {
+    "END" = '\33[0m',
+    "BOLD": '\33[1m',
+    "ITALIC": '\33[3m',
+    "URL": '\33[4m',
+    "BLINK": '\33[5m',
+    "BLINK2": '\33[6m',
+    "SELECTED": '\33[7m',
     "BLACK": '\33[30m',
     "RED": '\33[31m',
     "GREEN": '\33[32m',
