36e0456ac88729adfb199c52bd1d10a7c3a6f29f
MoPaMo <mpm1301@web.de>
2020-07-08 10:46:23 +0200

    added start for java version (in dev)

---
+++ printWithColor.java
@@ -0,0 +1,5 @@
+public class printWithColor {
+    public static void main(String[] args) {
+        System.out.println("\u001B[40mHello World!"+"\u001B[0m"); // Prints the string to the console.
+    }
+}
