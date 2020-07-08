4d1b03982c7af1f0c1dfe1d6e18dccd72ee3acc5
MoPaMo <mpm1301@web.de>
2020-07-08 17:15:59 +0200

    type null to use default color or font or bg color

--- printWithColor.java
+++ printWithColor.java
@@ -14,6 +14,7 @@ public class printWithColor {
 		colorValues.put("PURPLE", "\u001B[35m");
 		colorValues.put("CYAN", "\u001B[36m");
 		colorValues.put("WHITE", "\u001B[37m");
+		colorValues.put("none", "");		
 		bgValues.put("BLACK", "\u001B[40m");
 		bgValues.put("RED", "\u001B[41m");
 		bgValues.put("GREEN", "\u001B[42m");
@@ -22,13 +23,16 @@ public class printWithColor {
 		bgValues.put("PURPLE", "\u001B[45m");
 		bgValues.put("CYAN", "\u001B[46m");
 		bgValues.put("WHITE", "\u001B[47m");
+		bgValues.put("none", "");
 		alreadyBooted = true;
 	}
 
 	public static void printC(String term, String color, String bg) {
 		if (!alreadyBooted)
 			bootArray();
-		System.out.println(bgValues.get(bg) +colorValues.get(color) + term + "\u001B[0m"); // Prints the string to the console.
+		if(bg==null|bg=="") bg = "none";
+		if(color==null|color=="") color = "none";
+		System.out.println("\u001B[0m"+bgValues.get(bg) +colorValues.get(color) + term + "\u001B[0m");
 
 	}
 }
